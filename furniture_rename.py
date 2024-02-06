3a63dd706b1eecd0ed57dbb527649df2df8fc283
xnotyuki <85864531+xnotyuki@users.noreply.github.com>
2024-02-06 10:48:47 +0800

    Update furniture_rename.py
    Added base64 decoding and accounting for other case instances

--- furniture_rename.py
+++ furniture_rename.py
@@ -1,37 +1,60 @@
 import os
+import base64
 
 # huge bastardisation of reddu ylime and bluemorpho's codes into 1
-# renames fresh furniture files "furniture_number_l" to
-# wiki format "Furniture_Number"
+# renames fresh furniture files "furniture_number_l" to wiki format "Furniture_Number"
 
 # py furniture_rename.py
 
 folder = r"C:\Program Files\Python 3.9\furniture"
 # folder of your choice, must include r in front (at least for me :( )
 
-# if mass renaming, for checking how many files were already named
-count = 0
+# if mass renaming, for checking types of files processed
+alreadyexist = 0
+pngtopnglist = []
+invalidfile = []
+decodedlist = []
 
 for file in os.listdir(folder):
         filename = os.path.splitext(file)[0]
-# checks if each file is named according to the raw format + ends with .png
-        if filename.startswith("furniture") == True and file.endswith(".png"):
+# case 1: file matches raw format (prefix, length, digitlength) + already decoded
+        if filename.startswith("furniture_") and len(filename) > 16 and filename[10:16].isdigit() and file.endswith(".png"):
                 try:
                         old_name = os.path.join(folder, file)
-# yeets the _l and makes it the correct format
                         filename = filename.replace("_l", "")
                         new_name = os.path.join(folder, filename.capitalize() + ".png")
                         os.rename(old_name, new_name)
+                        pngtopnglist.append(filename.capitalize() + ".png")
                 except IOError:
                         pass
-# checks if file already renamed, length check (Furniture_1234567)
-        elif len(file) == 21:
-                count = count + 1
-                print("Already renamed.")
+# case 2: file matches raw format (prefix, length, digitlength)  + not decoded
+        elif filename.startswith("furniture_") and len(filename) > 16 and filename[10:16].isdigit() and file.endswith(".txt"):
+                imageData = open(folder + "\\" + file, "r").read()
+                decodedData = base64.b64decode(imageData)
+                filename = filename.replace("_l", "")
+                filename = filename.capitalize()
+                if os.path.exists(os.path.join(folder, filename + ".png")):#deletes _parsed file if it already exists
+                        pass
+                else:
+                        imgFile = open(folder + "\\" + filename.removesuffix(".txt") + ".png", "wb")
+                        imgFile.write(decodedData)
+                        imgFile.close()
+                        decodedlist.append(filename)
+# case 3: file already renamed (capitalised prefix, length, digitlength, path exist) and decoded
+        elif filename.startswith("Furniture_") and len(filename) == 17 and filename[10:16].isdigit() and os.path.exists(os.path.join(folder, filename + ".png")):
+                alreadyexist = alreadyexist + 1
+                print("> ??? Already renamed.")
         else:
-                print("This ain't a raw furniture file.")
-
-# Check
+                invalidfile.append(filename)
+                print("> !!! This ain't a raw furniture file.")
+                
+# Number of files: already named, .png renames, .txt renames, invalid files
 res = os.listdir(folder)
 print(res)
-print("Already named file count: " + str(count))
+print(">>>>>>> Already named file count: " + str(alreadyexist))
+print(">>>>>>> .png file rename count: " + str(len(pngtopnglist)))
+print(">>>>>>> .png files renamed: " + str(pngtopnglist))
+print(">>>>>>> Decoded file count: " + str(len(decodedlist)))
+print(">>>>>>> Decoded files: " + str(decodedlist))
+print(">>>>>>> Invalid file count: " + str(len(invalidfile)))
+print(">>>>>>> Invalid files: " + str(invalidfile))
